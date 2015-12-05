ab73351de0b21f5e1903c67bd7331a567a08b99e
Oudmane <ayoub@oudmane.me>
2015-12-05 04:44:46 +0000

    Application class

---
+++ src/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OudyPlat;
+
+/**
+ * Main application class for invoking applications
+ */
+class Application {
+
+    /**
+     *
+     * @var Page
+     */
+    public $page = null;
+
+    public function __construct() {
+        
+    }
+}
