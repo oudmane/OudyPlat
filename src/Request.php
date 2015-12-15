ee1210e0959b269b73d57e6a2e8e6c4b62614567
Oudmane <ayoub@oudmane.me>
2015-12-15 20:27:39 +0000

    functions of Request

--- src/Request.php
+++ src/Request.php
@@ -8,6 +8,17 @@ namespace OudyPlat;
  * @author Ayoub Oudmane <ayoub at oudmane.me>
  */
 class Request {
+    /**
+     *
+     * @var URL
+     */
+    public static $url = null;
+    public static function getBody() {
+        return file_get_contents('php://input');
+    }
+    public static function getJSONBody() {
+        return json_decode(self::getBody());
+    }
     public static function getCookie($name, $destroy = false) {
         if(!isset($_COOKIE[$name]))
             return null;
@@ -25,5 +36,50 @@ class Request {
     public static function destroyCookie($name) {
         setcookie($name, null, -1);
 		unset($_COOKIE[$name]);
+    }
+    public static function issetParam($param, $from = array('POST', 'GET', 'PATH')) {
+        foreach($from as $source) {
+            $function = 'isset'.ucfirst(strtolower($source));
+            return self::$function($param);
+        }
+        return false;
+    }
+    public static function getParam($param, $from = array('POST', 'GET', 'PATH')) {
+        foreach($from as $source) {
+            $function = 'isset'.ucfirst(strtolower($source));
+            if(self::$function($param)) {
+                $function = 'get'.ucfirst(strtolower($source));
+                return self::$function($param);
+            }
+        }
+        return false;
+    }
+    public static function issetPost($key) {
+        return isset($_POST[$key]);
+    }
+    public static function getPost($key) {
+        if(self::issetPost($key)) {
+            return $_POST[$key];
+        } else
+            return false;
+    }
+    public static function issetGet($key) {
+        return isset($_GET[$key]);
+    }
+    public static function getGet($key) {
+        if(self::issetGet($key)) {
+            return $_GET[$key];
+        } else
+            return false;
+    }
+    public static function issetPath($key) {
+        if(is_null(self::$url))
+            self::$url = new URL($_SERVER['REQUEST_URI']);
+        return self::$url->inPath($key);
+    }
+    public static function getPath($key) {
+        if(is_null(self::$url))
+            self::$url = new URL($_SERVER['REQUEST_URI']);
+        return self::$url->inPath($key, true);
     }
 }
