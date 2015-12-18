7bd694f5a2a2b68832d744a4d4b1994d043d74b2
Oudmane <ayoub@oudmane.me>
2015-12-18 17:34:56 +0000

    sanitze()

--- src/Application.php
+++ src/Application.php
@@ -72,6 +72,7 @@ class Application extends Object {
             $this->error(2503);
         
         $data =& $this->page->data;
+//        $this->setHeader('oudyplat');
         switch($position) {
             case 'api':
                 $notyet = false;
@@ -94,14 +95,20 @@ class Application extends Object {
                 break;
             case 'html':
                 $page =& $this->page;
+                $template = new Template();
+                $template->mergeClasses($page->classes);
+                $this->setHeader('html');
                 include ROOT_PATH.'templates/wetud/layouts/html.php';
+                break;
+            case 'module':
+            case 'view':
+                
                 break;
             default:
                 $this->setHeader('json');
                 echo json_encode($this, JSON_PRETTY_PRINT);
                 break;
         }
-        $this->setHeader('oudyplat');
     }
     public function loadByComponent($component, $task = null, $data = null) {
         $page = new Page(array(
@@ -186,11 +193,124 @@ class Application extends Object {
         );
         header($http[$header]);
     }
-
+    public function sanitaze() {
+        ob_start(function($input) {
+            if(trim($input) === "") return $input;
+            // Remove extra white-space(s) between HTML attribute(s)
+            $input = preg_replace_callback('#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s', function($matches) {
+                return '<' . $matches[1] . preg_replace('#([^\s=]+)(\=([\'"]?)(.*?)\3)?(\s+|$)#s', ' $1$2', $matches[2]) . $matches[3] . '>';
+            }, str_replace("\r", "", $input));
+            // Minify inline CSS declaration(s)
+            if(strpos($input, ' style=') !== false) {
+                $input = preg_replace_callback('#<([^<]+?)\s+style=([\'"])(.*?)\2(?=[\/\s>])#s', function($matches) {
+                    return '<' . $matches[1] . ' style=' . $matches[2] . minify_css($matches[3]) . $matches[2];
+                }, $input);
+            }
+            return preg_replace(
+                array(
+                    // t = text
+                    // o = tag open
+                    // c = tag close
+                    // Keep important white-space(s) after self-closing HTML tag(s)
+                    '#<(img|input)(>| .*?>)#s',
+                    // Remove a line break and two or more white-space(s) between tag(s)
+                    '#(<!--.*?-->)|(>)(?:\n*|\s{2,})(<)|^\s*|\s*$#s',
+                    '#(<!--.*?-->)|(?<!\>)\s+(<\/.*?>)|(<[^\/]*?>)\s+(?!\<)#s', // t+c || o+t
+                    '#(<!--.*?-->)|(<[^\/]*?>)\s+(<[^\/]*?>)|(<\/.*?>)\s+(<\/.*?>)#s', // o+o || c+c
+                    '#(<!--.*?-->)|(<\/.*?>)\s+(\s)(?!\<)|(?<!\>)\s+(\s)(<[^\/]*?\/?>)|(<[^\/]*?\/?>)\s+(\s)(?!\<)#s', // c+t || t+o || o+t -- separated by long white-space(s)
+                    '#(<!--.*?-->)|(<[^\/]*?>)\s+(<\/.*?>)#s', // empty tag
+                    '#<(img|input)(>| .*?>)<\/\1>#s', // reset previous fix
+                    '#(&nbsp;)&nbsp;(?![<\s])#', // clean up ...
+                    '#(?<=\>)(&nbsp;)(?=\<)#', // --ibid
+                    // Remove HTML comment(s) except IE comment(s)
+                    '#\s*<!--(?!\[if\s).*?-->\s*|(?<!\>)\n+(?=\<[^!])#s'
+                ),
+                array(
+                    '<$1$2</$1>',
+                    '$1$2$3',
+                    '$1$2$3',
+                    '$1$2$3$4$5',
+                    '$1$2$3$4$5$6$7',
+                    '$1$2$3',
+                    '<$1$2',
+                    '$1 ',
+                    '$1',
+                    ""
+                ),
+            $input);
+        });
+        function minify_css($input) {
+            if(trim($input) === "") return $input;
+            return preg_replace(
+                array(
+                    // Remove comment(s)
+                    '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')|\/\*(?!\!)(?>.*?\*\/)|^\s*|\s*$#s',
+                    // Remove unused white-space(s)
+                    '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/))|\s*+;\s*+(})\s*+|\s*+([*$~^|]?+=|[{};,>~+]|\s*+-(?![0-9\.])|!important\b)\s*+|([[(:])\s++|\s++([])])|\s++(:)\s*+(?!(?>[^{}"\']++|"(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')*+{)|^\s++|\s++\z|(\s)\s+#si',
+                    // Replace `0(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)` with `0`
+                    '#(?<=[\s:])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)#si',
+                    // Replace `:0 0 0 0` with `:0`
+                    '#:(0\s+0|0\s+0\s+0\s+0)(?=[;\}]|\!important)#i',
+                    // Replace `background-position:0` with `background-position:0 0`
+                    '#(background-position):0(?=[;\}])#si',
+                    // Replace `0.6` with `.6`, but only when preceded by `:`, `,`, `-` or a white-space
+                    '#(?<=[\s:,\-])0+\.(\d+)#s',
+                    // Minify string value
+                    '#(\/\*(?>.*?\*\/))|(?<!content\:)([\'"])([a-z_][a-z0-9\-_]*?)\2(?=[\s\{\}\];,])#si',
+                    '#(\/\*(?>.*?\*\/))|(\burl\()([\'"])([^\s]+?)\3(\))#si',
+                    // Minify HEX color code
+                    '#(?<=[\s:,\-]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3#i',
+                    // Replace `(border|outline):none` with `(border|outline):0`
+                    '#(?<=[\{;])(border|outline):none(?=[;\}\!])#',
+                    // Remove empty selector(s)
+                    '#(\/\*(?>.*?\*\/))|(^|[\{\}])(?:[^\s\{\}]+)\{\}#s'
+                ),
+                array(
+                    '$1',
+                    '$1$2$3$4$5$6$7',
+                    '$1',
+                    ':0',
+                    '$1:0 0',
+                    '.$1',
+                    '$1$3',
+                    '$1$2$4$5',
+                    '$1$2$3',
+                    '$1:0',
+                    '$1$2'
+                ),
+            $input);
+        }
+        function minify_js($input) {
+            if(trim($input) === "") return $input;
+            return preg_replace(
+                array(
+                    // Remove comment(s)
+                    '#\s*("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')\s*|\s*\/\*(?!\!|@cc_on)(?>[\s\S]*?\*\/)\s*|\s*(?<![\:\=])\/\/.*(?=[\n\r]|$)|^\s*|\s*$#',
+                    // Remove white-space(s) outside the string and regex
+                    '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/)|\/(?!\/)[^\n\r]*?\/(?=[\s.,;]|[gimuy]|$))|\s*([!%&*\(\)\-=+\[\]\{\}|;:,.<>?\/])\s*#s',
+                    // Remove the last semicolon
+                    '#;+\}#',
+                    // Minify object attribute(s) except JSON attribute(s). From `{'foo':'bar'}` to `{foo:'bar'}`
+                    '#([\{,])([\'])(\d+|[a-z_][a-z0-9_]*)\2(?=\:)#i',
+                    // --ibid. From `foo['bar']` to `foo.bar`
+                    '#([a-z0-9_\)\]])\[([\'"])([a-z_][a-z0-9_]*)\2\]#i'
+                ),
+                array(
+                    '$1',
+                    '$1$2',
+                    '}',
+                    '$1$3',
+                    '$1.$3'
+                ),
+            $input);
+        }
+    }
     public static function check() {
         if(!defined('ROOT_PATH'))
             die('ROOT_PATH undefined');
         if(!defined('COMPONENTS_PATH'))
             define('COMPONENTS_PATH', ROOT_PATH.'components'.DIRECTORY_SEPARATOR);
+        if(!defined('TEMPLATES_PATH'))
+            define('TEMPLATES_PATH', ROOT_PATH.'templates'.DIRECTORY_SEPARATOR);
     }
 }
