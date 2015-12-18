bf853d6b8664a06379c524d54998a85bd7422bde
Oudmane <ayoub@oudmane.me>
2015-12-18 17:36:43 +0000

    __construct, load, mergeClasses and getClass functions

--- src/Template.php
+++ src/Template.php
@@ -14,11 +14,35 @@ class Template extends Object {
     public $style = '';
     public $layout = 'html';
     public $version = '0.0.0';
-    public function __construct($data) {
-        if(self::$configuration) {
-            $template = self::$configuration->name;
-            parent::__construct($template);
+    public $classes = array();
+    public function __construct($data = null) {
+        if(self::$configuration)
+            $this->load(self::$configuration->name);
+        
+        if($data)
+            parent::__construct($data);
+    }
+    public function load($name) {
+        if(file_exists($template = TEMPLATES_PATH.$name.DIRECTORY_SEPARATOR.'template.json'))
+            parent::__construct(json_decode(file_get_contents($template)));
+        else
+            die($name.' template not installed');
+    }
+    public function mergeClasses($classes) {
+        foreach ($classes as $position => $classes) {
+            if(isset($classes->set))
+                $this->classes->$position = $classes->set;
+            if(isset($classes->add))
+                $this->classes->$position = array_unique(array_merge($this->classes->$position, $classes->add));
         }
-        parent::__construct($data);
+    }
+    public function getClass($position) {
+        if(isset($this->classes->$position))
+            if(in_array('uk-hidden', $this->classes->$position))
+                return 'uk-hidden';
+            else
+                return implode(' ', $this->classes->$position);
+        else
+            return '';
     }
 }
