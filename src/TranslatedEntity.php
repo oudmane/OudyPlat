6f244d49d071b9b161e3ead32b13dc24ec483aa1
Ayoub Oudmane <ayoub@oudmane.me>
2016-07-11 17:44:06 +0100

    default language of Object

--- src/TranslatedEntity.php
+++ src/TranslatedEntity.php
@@ -3,7 +3,18 @@
 namespace OudyPlat;
 
 class TranslatedEntity extends Entity {
+    private $_language = '';
+    public function setLanguage($language) {
+        if(empty($this->_language))
+            $this->_language = Language::$defaultLanguage;
+        $this->_language = $language;
+    }
+    public function getLanguage() {
+        return $this->_language;
+    }
     public function get($property, $language = null) {
+        if(empty($language))
+            $language = $this->getLanguage();
         $key = $language.ucfirst($property);
         return (isset($this->$key) && $this->$key) ? $this->$key : $this->$property;
     }
